8e2cb38c196347c0227cf96bd90f2e09afea74a7
avdser <avdser@yandex.ru>
2017-12-26 22:01:01 +0700

    Add files via upload

---
+++ github.bat
@@ -0,0 +1,6 @@
+echo "# ocvoi.github.com" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/avdser/ocvoi.github.com.git
+git push -u origin master
